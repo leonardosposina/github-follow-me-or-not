96d5ee592940ee200287768c2afd0d6cf4c1151b
Leonardo Sposina <leonardo.sposina@icloud.com>
2020-08-16 15:17:17 -0300

    Updates

---
+++ precache-manifest.01be02d2398d040c8e697c7bf5430982.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f3627856fcc8e8283a7a5d2bb4846",
+    "url": "/github-follow-me-or-not/index.html"
+  },
+  {
+    "revision": "36ddf7861b69d25d1952",
+    "url": "/github-follow-me-or-not/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e51dcace05f5ee56f8ba",
+    "url": "/github-follow-me-or-not/static/css/main.4ca7e300.chunk.css"
+  },
+  {
+    "revision": "36ddf7861b69d25d1952",
+    "url": "/github-follow-me-or-not/static/js/2.be920d1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-follow-me-or-not/static/js/2.be920d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51dcace05f5ee56f8ba",
+    "url": "/github-follow-me-or-not/static/js/main.aadded62.chunk.js"
+  },
+  {
+    "revision": "36cc649a81efb778d5c5",
+    "url": "/github-follow-me-or-not/static/js/runtime-main.91de8212.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/github-follow-me-or-not/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "5fa3cc5b7ba22d72da8e3f41dd64adb9",
+    "url": "/github-follow-me-or-not/static/media/octocat.5fa3cc5b.svg"
+  }
+]);
